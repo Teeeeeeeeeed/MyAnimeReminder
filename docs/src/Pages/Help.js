20428dc7ee8605cc262afa678bb32429679509bd
Teeeeeeeeeed <ted.te.lo@hotmail.com>
2021-04-12 17:28:25 +1200

    2nd commit

---
+++ docs/src/Pages/Help.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Layout} from '../Layout';
+import {Helmet} from 'react-helmet';
+import Navi from '../Layout/Navi';
+import Footer from '../Layout/Footer';
+
+const Help=()=>
+<Layout>
+    <Helmet><title>MAR - Help</title></Helmet>
+    <div className='Help'>
+        <Navi/>
+        <Footer/>
+    </div>
+</Layout>;
+
+export default Help;
---
+++ docs/src/Pages/index.js
@@ -0,0 +1,7 @@
+import Calender from './Calender';
+import About from './About';
+import Trending from './Trending';
+import Help from './Help';
+import Profile from './Profile';
+
+export { Calender, About, Trending, Help, Profile };
